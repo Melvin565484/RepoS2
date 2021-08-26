5d6c0fc30ea33fdaaed19b80ecd44874e08ba890
Melvin Mendoza <melsmendoza17@gmail.com>
2021-08-26 12:42:26 -0300

    Semana 2 ejercicio 3

---
+++ enteros/enteros/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enteros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa  que  pida  al  usuario  dos
+            //números  enteros.  Si  el  segundo  no  es  cero, mostrará el resultado de dividir
+            //entre el primero y el segundo. Por el contrario, si el segundo número es cero,
+            //escribirá "Error: No se puede dividir entre cero".
+
+            Int32 nentero;
+            Int32 nentero1;
+            int resultado;
+
+            Console.WriteLine("Ingrese valor entero: ");
+            nentero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese valor entero: ");
+            nentero1 = Convert.ToInt32(Console.ReadLine());
+
+         
+
+            if(nentero1 > 0)
+            {
+                resultado = (nentero / nentero1);
+                Console.WriteLine("Resultado de operacion: ", resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero...");
+            }
+            Console.ReadKey();
+        }
+    }
+}
